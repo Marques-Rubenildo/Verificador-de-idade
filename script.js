b3dec49b6bba80a0e4ee1403d32372292c31d7c3
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-05-05 12:38:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function verificar() {
     var ano = data.getFullYear()
     var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    
-    
     if(fAno.value.length == 0 || fAno.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente')
     } else {
@@ -39,22 +37,22 @@ function verificar() {
             genero = 'mulher'
             if(idade >= 0 && idade < 4) {
                 //Bêbe
-                img.setAttribute('src', 'baby-girl.png')
+                img.setAttribute('src', 'img/baby-girl.png')
             } else if (idade >= 5 && idade < 11) {
                 //Criança
-                img.setAttribute('src', 'child-girl.png')
+                img.setAttribute('src', 'img/child-girl.png')
             } else if (idade >= 11 && idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'young-girl.png')
+                img.setAttribute('src', 'img/young-girl.png')
             } else if (idade >= 21 && idade < 40) {
                 //Adulto
-                img.setAttribute('src', 'mulher-adulta.png')
+                img.setAttribute('src', 'img/mulher-adulta.png')
             } else if (idade >= 40 && idade < 60) {
                 //Maduro
-                img.setAttribute('src', 'mulher-madura.png')
+                img.setAttribute('src', 'img/mulher-madura.png')
             } else {
                 //Idoso
-                img.setAttribute('src', 'elder-woman.png')
+                img.setAttribute('src', 'img/elder-woman.png')
             }
         }
         res.style.textAlign = 'center'
