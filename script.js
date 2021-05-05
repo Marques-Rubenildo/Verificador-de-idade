7cc2e7e422b2ccdeb7a96b5e2b24ab56471f0f21
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-05-05 12:14:54 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,64 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fAno = document.querySelector('p#txtano')
+    var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+    
+    
+    if(fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        //res.innerHTML = `Indade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if(idade >= 0 && idade < 4) {
+                //Bêbe
+                img.setAttribute('src', 'img/baby-boy.png')
+            } else if (idade >= 5 && idade < 11) {
+                //Criança
+                img.setAttribute('src', 'img/child-boy.png')
+            } else if (idade >= 11 && idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'img/young-boy.png')
+            } else if (idade >= 21 && idade < 40) {
+                //Adulto
+                img.setAttribute('src', 'img/homem-adulto.png')
+            } else if (idade >= 40 && idade < 60) {
+                //Maduro
+                img.setAttribute('src', 'img/homem-maduro.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'img/elder-man.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if(idade >= 0 && idade < 4) {
+                //Bêbe
+                img.setAttribute('src', 'baby-girl.png')
+            } else if (idade >= 5 && idade < 11) {
+                //Criança
+                img.setAttribute('src', 'child-girl.png')
+            } else if (idade >= 11 && idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'young-girl.png')
+            } else if (idade >= 21 && idade < 40) {
+                //Adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else if (idade >= 40 && idade < 60) {
+                //Maduro
+                img.setAttribute('src', 'mulher-madura.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'elder-woman.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = ` Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
 }
